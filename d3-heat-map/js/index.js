b23c987933400f8c7feddb8c8a692e36fac25659
Evgenii Dergachev <edergachev@bk.ru>
2019-05-10 12:08:07 +1000

    added d3-heat-map axes

--- d3-heat-map/js/index.js
+++ d3-heat-map/js/index.js
@@ -9,5 +9,34 @@ document.addEventListener("DOMContentLoaded", async () => {
   else {
     dataset = JSON.parse(localStorage.getItem('data-d3-heat-map'))
   }
-  console.log(dataset)
+  const height = 650, width = 1200
+  const xPadding = 75, yPadding  = 110
+  let years = []
+  dataset.monthlyVariance.forEach(el => years.push(el.year))
+  years = years.filter((v, i, a) => a.indexOf(v) === i)
+  const maxYear = d3.max(years), minYear = d3.min(years)
+  const xScale = d3.scaleLinear()
+                   .domain([minYear, maxYear])
+                   .range([0, 1070])
+  const xAxis = d3.axisBottom(xScale)
+                  .tickFormat(d3.format("d"))
+  const months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  const yScale = d3.scaleBand()
+                   .domain(months)
+                   .range([height - yPadding, 20])
+  const yAxis = d3.axisLeft(yScale)
+                  .tickFormat(month => monthNames[month])
+  const svg = d3.select("#heat-map")
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height)
+  svg.append("g")
+     .attr("transform", "translate(" + xPadding + "," + (height - yPadding) + ")")
+     .call(xAxis)
+     .attr('id', 'x-axis')
+  svg.append("g")
+     .attr("transform", "translate(" + xPadding + "," + 0 + ")")
+     .call(yAxis)
+     .attr('id', 'y-axis')  
 })
