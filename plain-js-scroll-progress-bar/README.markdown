694cb5885078c5d4e862d5539408b6459214afdb
Evgenii Dergachev <edergachev@bk.ru>
2019-09-08 23:17:57 +1000

    added scroll progress bar

---
+++ plain-js-scroll-progress-bar/README.markdown
@@ -0,0 +1,4 @@
+# Scroll Progress Bar
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/ead/pen/gOYvGmy](https://codepen.io/ead/pen/gOYvGmy).
+
+ 
---
+++ plain-js-scroll-progress-bar/script.js
@@ -0,0 +1,9 @@
+//the code is taken from https://eloquentjavascript.net/15_event.html where all the code samples are MIT licensed
+document.body.appendChild(document.createTextNode(
+    "supercalifragilisticexpialidocious ".repeat(1000)));
+
+  let bar = document.querySelector("#progress");
+  window.addEventListener("scroll", () => {
+    let max = document.body.scrollHeight - innerHeight;
+    bar.style.width = `${(pageYOffset / max) * 100}%`;
+  });
