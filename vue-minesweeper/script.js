e79710007f0ed7d093a13cf20369faec1501b0e4
Evgenii Dergachev <edergachev@bk.ru>
2019-12-28 23:14:30 +1000

    added opening and marking the field to minesweaper

--- vue-minesweeper/script.js
+++ vue-minesweeper/script.js
@@ -45,28 +45,77 @@ for (let i = 0; i < 9; i++) {
     }
 }
 
-const temp = field.reduce((acc, val) => acc.concat(val), []);
+let temp = field.reduce((acc, val) => acc.concat(val), []);
+temp = temp.map(val => {return {content: val, hidden: true, marked: false}});
 
 Vue.component("cell", {
-  props: ["content", "index"],
+  props: ["content", "index", "hidden", "marked"],
+  data: function() {
+    return {val: ""};
+  },
+  beforeUpdate() {
+    this.val = this.marked ? "🚩" : this.hidden ? "" : this.content;
+  },
   computed: {
     style() {
-      const color = ([1, 2, 3, 4, 5, 6, 7, 8].includes(this.content) ? colors[this.content] : 'black');
+      const backgroundColor = this.hidden ? "silver" : "whitesmoke";
+      const color = this.marked ? "red" : ([1, 2, 3, 4, 5, 6, 7, 8].includes(this.content) ? colors[this.content] : 'black');
       if (this.index > 0 && (this.index + 1) % 9 === 0) {
-        console.log(this.index);
-        return {color};   
+        return {color, backgroundColor};   
       }
       else {
-        return {borderRight: "1px solid black", color}; 
+        return {borderRight: "1px solid black", color, backgroundColor}; 
       }
     }
   },
-  template: '<div class="cell" v-bind:style="style">{{ content }}</div>'
+  template: `<div 
+               class="cell" 
+               :style="style" 
+               @click.left="$emit('disclose-cell')"
+               @click.right="$emit('mark-cell')"
+             >{{ this.val }}</div>`
 });
 
 const app = new Vue({
   el: "#field-box",
-  data: {
-    field: temp
+  data: function() {
+        return {field: temp};
+  },
+  methods: {
+    discloseCell: function(index) {
+      const calculateVicinity = (i) => {
+          let vicinity;
+          if ([0, 9, 18, 27, 36, 45, 54, 63, 72].includes(i)) {
+            vicinity = [i + 1, i - 8, i - 9, i + 9, i + 10];  
+          }
+          else if ([8, 17, 26, 35, 44, 53, 62, 71, 80].includes(i)) {
+            vicinity = [i - 1, i - 9, i - 10, i + 8, i + 9];  
+          }
+          else {
+            vicinity = [i - 1, i + 1, i - 8, i - 9, i - 10, i + 8, i + 9, i + 10];  
+          }
+          vicinity = vicinity.filter(vIndex => vIndex >= 0 && vIndex <= 80);
+          return vicinity;
+      }
+      if (!this.field[index].marked) {
+        this.field[index].hidden = false;
+        if (this.field[index].content === "") {
+          let vicinity = calculateVicinity(index);
+          while (vicinity.length !== 0) {
+            vicinity.forEach(vIndex => this.field[vIndex].hidden = this.field[vIndex].marked ? true : false);
+            vicinity = vicinity.filter(vIndex => this.field[vIndex].content === "");
+            let temp = [];
+            vicinity.forEach(vIndex => temp.push(calculateVicinity(vIndex)));
+            vicinity = temp.reduce((acc, val) => acc.concat(val), []).filter(vIndex => this.field[vIndex].hidden);
+            vicinity = [...new Set(vicinity)];
+          }
+        }
+      }
+    },
+    markCell: function(index) {
+      if (this.field[index].hidden) {
+        this.field[index].marked = this.field[index].marked ? false : true;   
+      }
+    }
   },
 });
