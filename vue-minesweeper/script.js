4b86d0ed97376b9c728b8cbc5a576d26e2a67b64
Evgenii Dergachev <edergachev@bk.ru>
2020-01-05 00:38:17 +1000

    finished gameplay

--- vue-minesweeper/script.js
+++ vue-minesweeper/script.js
@@ -46,20 +46,20 @@ for (let i = 0; i < 9; i++) {
 }
 
 let temp = field.reduce((acc, val) => acc.concat(val), []);
-temp = temp.map(val => {return {content: val, hidden: true, marked: false}});
+temp = temp.map(val => {return {content: val, hidden: true, marked: false, questioned: false}});
 
 Vue.component("cell", {
-  props: ["content", "index", "hidden", "marked"],
+  props: ["content", "index", "hidden", "marked", "questioned"],
   data: function() {
     return {val: ""};
   },
   beforeUpdate() {
-    this.val = this.marked ? "🚩" : this.hidden ? "" : this.content;
+    this.val = this.marked ? "🚩" : (this.questioned && this.hidden) ? "❓" : this.hidden ? "" : this.content;
   },
   computed: {
     style() {
       const backgroundColor = this.hidden ? "silver" : "whitesmoke";
-      const color = this.marked ? "red" : ([1, 2, 3, 4, 5, 6, 7, 8].includes(this.content) ? colors[this.content] : 'black');
+      const color = this.marked ? "red" : (this.questioned && this.hidden) ? "dimgrey" : ([1, 2, 3, 4, 5, 6, 7, 8].includes(this.content) ? colors[this.content] : 'black'); //the last black is for the mines
       if (this.index > 0 && (this.index + 1) % 9 === 0) {
         return {color, backgroundColor};   
       }
@@ -79,42 +79,83 @@ Vue.component("cell", {
 const app = new Vue({
   el: "#field-box",
   data: function() {
-        return {field: temp};
+        return {field: temp, isLost: false, isWon: false};
+  },
+  beforeUpdate() {
+    let numHidden = 0;
+    for (let cell of this.field) {
+      if (cell.hidden) numHidden++;
+    }
+    if (numHidden === 10) {
+      this.isWon = true;
+      for (let cell of this.field) {
+        if (cell.hidden && !cell.marked) {
+          cell.marked = true;
+        }
+      }      
+    }
   },
   methods: {
-    discloseCell: function(index) {
-      const calculateVicinity = (i) => {
-          let vicinity;
-          if ([0, 9, 18, 27, 36, 45, 54, 63, 72].includes(i)) {
-            vicinity = [i + 1, i - 8, i - 9, i + 9, i + 10];  
-          }
-          else if ([8, 17, 26, 35, 44, 53, 62, 71, 80].includes(i)) {
-            vicinity = [i - 1, i - 9, i - 10, i + 8, i + 9];  
-          }
-          else {
-            vicinity = [i - 1, i + 1, i - 8, i - 9, i - 10, i + 8, i + 9, i + 10];  
-          }
-          vicinity = vicinity.filter(vIndex => vIndex >= 0 && vIndex <= 80);
-          return vicinity;
+    calculateVicinity: function(i) {
+      let vicinity;
+      if ([0, 9, 18, 27, 36, 45, 54, 63, 72].includes(i)) {
+        vicinity = [i + 1, i - 8, i - 9, i + 9, i + 10];  
       }
-      if (!this.field[index].marked) {
-        this.field[index].hidden = false;
-        if (this.field[index].content === "") {
-          let vicinity = calculateVicinity(index);
-          while (vicinity.length !== 0) {
-            vicinity.forEach(vIndex => this.field[vIndex].hidden = this.field[vIndex].marked ? true : false);
-            vicinity = vicinity.filter(vIndex => this.field[vIndex].content === "");
-            let temp = [];
-            vicinity.forEach(vIndex => temp.push(calculateVicinity(vIndex)));
-            vicinity = temp.reduce((acc, val) => acc.concat(val), []).filter(vIndex => this.field[vIndex].hidden);
-            vicinity = [...new Set(vicinity)];
-          }
+      else if ([8, 17, 26, 35, 44, 53, 62, 71, 80].includes(i)) {
+        vicinity = [i - 1, i - 9, i - 10, i + 8, i + 9];  
+      }
+      else {
+        vicinity = [i - 1, i + 1, i - 8, i - 9, i - 10, i + 8, i + 9, i + 10];  
+      }
+      vicinity = vicinity.filter(vIndex => vIndex >= 0 && vIndex <= 80);
+      return vicinity;   
+    },
+    riskyBunch: function(i) {
+      const bombsNumber = Number(this.field[i].content); //clicking on a bomb be made impossible with gameover
+      const vicinity = this.calculateVicinity(i);
+      let numMarked = 0;
+      for (let vIndex of vicinity) {
+        if (this.field[vIndex].marked) numMarked++;
+      }
+      if (numMarked === bombsNumber) {
+        for (let vIndex of vicinity) {
+          this.discloseCell(vIndex, true)
         }
       }
     },
-    markCell: function(index) {
+    discloseCell: function(index, hiddenOnly) {
+      if (this.isLost || this.isWon) return;
       if (this.field[index].hidden) {
-        this.field[index].marked = this.field[index].marked ? false : true;   
+        if (!this.field[index].marked) {
+          this.field[index].hidden = false;
+          if (this.field[index].content === "") {
+            let vicinity = this.calculateVicinity(index);
+            while (vicinity.length !== 0) {
+              vicinity.forEach(vIndex => this.field[vIndex].hidden = this.field[vIndex].marked ? true : false);
+              vicinity = vicinity.filter(vIndex => this.field[vIndex].content === "");
+              let temp = [];
+              vicinity.forEach(vIndex => temp.push(this.calculateVicinity(vIndex)));
+              vicinity = temp.reduce((acc, val) => acc.concat(val), []).filter(vIndex => this.field[vIndex].hidden);
+              vicinity = [...new Set(vicinity)];
+            }
+          }
+          else if (this.field[index].content === "💣") {
+            this.isLost = true;
+          }
+        }
+      }
+      else if (hiddenOnly !== true) {
+        this.riskyBunch(index);
+      }
+    },
+    markCell: function(index) {
+      if (this.isLost || this.isWon) return;
+      if (this.field[index].hidden) {
+        this.field[index].marked = (!this.field[index].marked && !this.field[index].questioned) ? true : false;
+        this.field[index].questioned = (this.field[index].marked || this.field[index].questioned) ? false : true;
+      }
+      else {
+        this.riskyBunch(index);
       }
     }
   },
